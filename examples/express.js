d69290e88fadcdc81f81b97331c080b0f4df22b4
renancouto <renancouto@gmail.com>
2013-06-20 16:06:08 -0300

    Minor changes, nothing on the lib itself

--- examples/express.js
+++ examples/express.js
@@ -8,20 +8,8 @@ app.engine('html', ulayout.__express);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
-var users = [];
-users.push({ name: 'tobi' });
-users.push({ name: 'loki' });
-users.push({ name: 'jane' });
-
 app.get('/', function(req, res){
   res.render('index', { title: 'Title', text: 'Text' });
-});
-
-app.get('/users', function(req, res){
-  res.render('users', {
-    title: 'Users',
-    users: users
-  });
 });
 
 app.listen(3000);
